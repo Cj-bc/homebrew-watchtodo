56088393140ee43def26a2b2301cad7e04d77ddb
Cj-bc <cj-bc@y-modify.org>
2018-06-04 10:11:26 +0900

    Add: init

---
+++ watchtodo.rb
@@ -0,0 +1,19 @@
+class Watchtodo < Formula
+  desc "watch todo on your directory, in other tty"
+  homepage "https://github.com/Cj-bc/watchtodo"
+  version "0.3.0"
+  url "https://github.com/Cj-bc/watchtodo/archive/0.3.0.tar.gz"
+
+
+  def install
+    system "mv", "watchtodo.sh", "watchtodo"
+    bin.install "watchtodo"
+  end
+
+  def caveats; <<~EOT
+    please add below to `~/.bashrc` or something source it.
+    ```
+      export PS1="${PS1} \$(echo \$(pwd) >~/.watchtodo/pwd 2>/dev/null)"
+    ```
+    EOT
+  end
